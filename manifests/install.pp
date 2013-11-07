e630d85a1d11fd2c9f9616731a43cfa569123db4
Simon Piette <simon.piette@savoirfairelinux.com>
2013-11-06 19:41:41 -0500

    set a proper logrotate for default values

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,17 @@
 # == Class redis::intall
-# This class is meant to be called from redis
-# It install requires packages
+# This class is meant to be called from redis. It installs required packages
+# and a default logrotate file.
 class redis::install {
   include redis::params
   package { $redis::params::pkgname:
     ensure => present,
+  }
+  file { '/etc/logrotate.d/redis':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    source  => 'puppet:///redis/redis.logrotate',
+    require => Package[$redis::params::pkgname],
   }
 }
