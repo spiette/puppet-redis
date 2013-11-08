bcc25c3f4de2ec3572acffa90ee0039adcf50616
Simon Piette <simon.piette@savoirfairelinux.com>
2013-11-08 10:47:11 -0500

    cover file creation and content in spec tests

--- spec/defines/redis_instance_spec.rb
+++ spec/defines/redis_instance_spec.rb
@@ -16,6 +16,7 @@ describe 'redis::instance', :type => 'define'  do
       it { should create_redis__instance(name) }
 
       it { should create_file("/etc/redis#{name}.conf")
+           .with_content(/^port 6379/ )\
            .with_content(/^logfile .var.log.redis.redis#{name}.log$/ )\
            .with_content(/^pidfile .var.run.redis.redis#{name}.pid$/ )\
            .with_content(/^dir .var.lib.redis#{name}$/ )\
@@ -24,11 +25,26 @@ describe 'redis::instance', :type => 'define'  do
            .with_content(/^timeout 0$/ )\
            .with_content(/^loglevel notice$/ )\
       }
-      it { should create_file("/etc/init.d/redis#{name}") }
+      it { should create_file("/etc/init.d/redis#{name}")\
+           .with_content(/^pidfile=".var.run.redis.redis#{name}.pid"$/ )\
+           .with_content(/^REDIS_CONFIG=".etc.redis#{name}.conf"$/ )
+      }
       it { should create_service("redis#{name}")
            .with_ensure('running')\
            .with_enable('true')\
            .with_hasstatus('true')
+      }
+      it { should create_file('/etc/logrotate.d/redis')\
+           .with_ensure('file')\
+           .with_owner('root')\
+           .with_group('root')\
+           .with_mode('0440')
+      }
+      it { should create_file("/var/lib/redis#{name}")\
+           .with_ensure('directory')\
+           .with_owner('redis')\
+           .with_group('root')\
+           .with_mode('0644')
       }
 
     end
